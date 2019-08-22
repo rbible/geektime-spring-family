430d8f0d96b1544fcb6a447b09a687515bf7ede8
renbing <beck.ren@qq.com>
2019-08-22 17:24:52 +0800

    format

--- Chapter 2/declarative-transaction-demo/src/main/java/geektime/spring/data/declarativetransactiondemo/DeclarativeTransactionDemoApplication.java
+++ Chapter 2/declarative-transaction-demo/src/main/java/geektime/spring/data/declarativetransactiondemo/DeclarativeTransactionDemoApplication.java
@@ -25,24 +25,16 @@ public class DeclarativeTransactionDemoApplication implements CommandLineRunner 
 	@Override
 	public void run(String... args) throws Exception {
 		fooService.insertRecord();
-		log.info("AAA {}",
-				jdbcTemplate
-						.queryForObject("SELECT COUNT(*) FROM FOO WHERE BAR='AAA'", Long.class));
+		log.info("AAA {}", jdbcTemplate.queryForObject("SELECT COUNT(*) FROM FOO WHERE BAR='AAA'", Long.class));
 		try {
 			fooService.insertThenRollback();
 		} catch (Exception e) {
-			log.info("BBB {}",
-					jdbcTemplate
-							.queryForObject("SELECT COUNT(*) FROM FOO WHERE BAR='BBB'", Long.class));
+			log.info("BBB {}", jdbcTemplate.queryForObject("SELECT COUNT(*) FROM FOO WHERE BAR='BBB'", Long.class));
 		}
-
 		try {
 			fooService.invokeInsertThenRollback();
 		} catch (Exception e) {
-			log.info("BBB {}",
-					jdbcTemplate
-							.queryForObject("SELECT COUNT(*) FROM FOO WHERE BAR='BBB'", Long.class));
+			log.info("BBB {}", jdbcTemplate.queryForObject("SELECT COUNT(*) FROM FOO WHERE BAR='BBB'", Long.class));
 		}
 	}
 }
-
